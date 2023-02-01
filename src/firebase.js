77850d574dd7c3f91a2a7c31589878939a0befce
ge5rg2 <kghee9612@gmail.com>
2023-02-01 23:37:28 +0900

    #1.2 setup env

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD5dtEVj1c3mRCX8G0INOruQZIGJweutq4",
-    authDomain: "wuri-81154.firebaseapp.com",
-    projectId: "wuri-81154",
-    storageBucket: "wuri-81154.appspot.com",
-    messagingSenderId: "1088210734631",
-    appId: "1:1088210734631:web:25dd0797b150e2b8124802"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
   // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);

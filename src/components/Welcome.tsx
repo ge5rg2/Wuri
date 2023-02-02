35cf03609618141c8ad01b6a41dd8806dd0a7d7d
ge5rg2 <kghee9612@gmail.com>
2023-02-02 18:43:11 +0900

    #1.5 edit Welcomepage

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,20 +1,50 @@
 import React, { useState } from "react";
 import Input from "./common/Input";
+import Btn from "./common/Btn";
 
 const Welcome = () => {
   const [userId, setUserId] = useState<string>();
-  const [userPassword, setUserPassword] = useState<number>();
+  const [userPassword, setUserPassword] = useState<any>();
 
-  const onSubmit = () => {
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     alert("completed!");
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name == "email") {
+      setUserId(value);
+    } else if (name == "pw") {
+      setUserPassword(value);
+    }
   };
 
   return (
     <>
       <form onSubmit={onSubmit}>
-        <Input label="E-mail" types="email" />
-        <Input label="Password" types="password" />
+        <Input
+          onChange={onChange}
+          value={userId}
+          name="email"
+          label="E-mail"
+          types="email"
+        />
+        <Input
+          onChange={onChange}
+          value={userPassword}
+          name="pw"
+          label="Password"
+          types="password"
+        />
+        <Input value="Log In" types="submit" />
       </form>
+      <div>
+        <Btn children="Continue with Google" />
+        <button>Continue with Github</button>
+      </div>
     </>
   );
 };

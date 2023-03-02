60a9d76859cf8e9f939fb6309bb9acaf4fe7a6f1
ge5rg2 <kghee9612@gmail.com>
2023-03-02 23:30:17 +0900

    #2.9 set up current users Info to Profile

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -2,16 +2,39 @@ import Nav from "../components/Nav";
 import { MainContainer } from "../styles/HomeStyle";
 import React, { useEffect } from "react";
 import { collection, getDocs, query, where } from "@firebase/firestore";
+import { dbService } from "../myBase";
+import { useSelector } from "../store";
+import { getAuth } from "firebase/auth";
 
 const Profile = () => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const userName = user?.displayName;
+  const url: any = user?.photoURL;
+  const provider = user?.providerData[0].providerId;
+  const uid = useSelector((state) => state.user.userUid);
+
+  const getMyAccount = async () => {
+    const q = query(
+      collection(dbService, "diarys"),
+      where("creatorId", "==", uid)
+    );
+    const querySnapshot = await getDocs(q);
+    /*     console.log(
+      querySnapshot.forEach((doc) => console.log(doc.id, " => ", doc.data()))
+    ); */
+  };
+
   useEffect(() => {
-    console.log("Hi");
+    getMyAccount();
   }, []);
 
   return (
     <MainContainer>
       <Nav />
-      <div>isProfile</div>
+      <img src={url + "-mo"} />
+      <div>{userName}</div>
+      <div>{provider}</div>
     </MainContainer>
   );
 };

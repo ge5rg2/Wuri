6955501dd72119fc7c02452a957c5d1f7514919a
ge5rg2 <kghee9612@gmail.com>
2023-03-09 18:49:44 +0900

    window added getDoc by id

--- src/routes/Edit.tsx
+++ src/routes/Edit.tsx
@@ -1,6 +1,26 @@
 import { MainContainer } from "../styles/HomeStyle";
+import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { dbService } from "../myBase";
+import { useEffect } from "react";
 
 const Edit = () => {
+  const param = useParams();
+
+  const getDiaryInfo = async () => {
+    const snap = await getDoc(doc(dbService, "diarys", "X71qs2yf1RcXcsPurPND"));
+
+    if (snap.exists()) {
+      console.log(snap.data());
+    } else {
+      console.log("No such document");
+    }
+  };
+
+  useEffect(() => {
+    getDiaryInfo();
+  }, []);
+
   return (
     <>
       <MainContainer>

a3a5267f15b53ec3b34477a52691351d777caee1
ge5rg2 <kghee9612@gmail.com>
2023-05-04 00:18:03 +0900

    add themes

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -5,4 +5,14 @@ declare module "styled-components" {
     bgColor: string;
     accentColor: string;
   }
+  export interface LightTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+  }
+  export interface DarkTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+  }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,19 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme, LightTheme, DarkTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   bgColor: "#2B2B2B",
   textColor: "#f5f6fa",
   accentColor: "#9c88ff",
+};
+
+export const lightTheme: LightTheme = {
+  bgColor: "#F2F2F2",
+  textColor: "#1A1A1A",
+  accentColor: "#E50915",
+};
+
+export const darkTheme: DarkTheme = {
+  bgColor: "#1A1A1A",
+  textColor: "#F2F2F2",
+  accentColor: "#E50915",
 };
